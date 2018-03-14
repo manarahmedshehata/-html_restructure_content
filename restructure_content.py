77719c3454ba551067e8b3643ba0f0ff42793b42
manarahmedshehata <manarahmedshehata@gmail.com>
2018-03-14 22:56:48 +0200

    add get h2

--- restructure_content.py
+++ restructure_content.py
@@ -1,12 +1,21 @@
 from bs4 import BeautifulSoup
 
-def test(filename):
+def restructure_content(filename):
 	file = open(filename, encoding="utf8").read()
 	# print(file)
+	out={"sections":[]}
+	sections=[]
+	#reead html file
 	parsed_html = BeautifulSoup(file, "html.parser")
 	print("#########################################")
+	#Get searchable-content 
 	html_content=parsed_html.body.find('div', attrs={'class':'body searchable-content'})
 	#print(html_content.find_all('h2'))
-	
+	for i in range(1,7):
+		if html_content.find_next('h'+ str(i)) != None:
+			print(i)
+			print(html_content.find_next('h'+ str(i)).text)
+			sections.append({"section":html_content.find_next('h'+ str(i)).text})
+			
 	print("#########################################")	
-test('C:/Users/HassanM/Desktop/manar/ma/inputs/5bd6c00d652cfa3480dff05398d0e4b6')
+restructure_content('inputs/5bd6c00d652cfa3480dff05398d0e4b6')
