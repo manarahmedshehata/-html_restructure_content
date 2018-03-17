e1ca6c23e989649526859d52585e2611be680a5e
manarahmedshehata <manarahmedshehata@gmail.com>
2018-03-17 17:04:02 +0200

    get section and check for subsections

--- restructure_content.py
+++ restructure_content.py
@@ -4,6 +4,7 @@ from pprint import pprint
 import re
 
 def restructure_content(filename):
+	#schema
 	"""
 	json={"sections":[
 			{
@@ -19,29 +20,70 @@ def restructure_content(filename):
 		]
 		}
 	"""
+	output_json = {'sections': []}
+
 	try:
 		file = open(filename, encoding="utf8")
 		# print(file)
 		parsed_html = BeautifulSoup(file.read(), "html.parser")
-		print("#########################################")
 		html_content=parsed_html.body.find('div', attrs={'class':'body searchable-content'}) 
+		next=html_content.find_next()
+		#print("################### Print Next ######################")
+		#print(next)
+		#print("#########################################")
+		while True:
+			print("while")
+			if not next:
+				break
+			#check if next is header
+			if re.match("^h", next.name):
+				print("next is header")
+				section = build_section(next)
+				print("################# Print Section ########################")
+				print(section)
+				print("#########################################")
+			else:
+				next = next.find_next()
+				print("################# Print next ########################")
+				print(next)
+				print("#########################################")
+
+
+
+
+
+		"""
 		if html_content.find_next(re.compile("^h")) != None:
 			print(html_content.find_next(re.compile("^h")))
 			build_section(html_content.find_next(re.compile("^h")))
+		"""
 	finally:
 		file.close()
 
 def build_section(header):
-	next=header
+	#section_obj to build output_json
+	print("build section function")
+	section_obj= {}
+	next=header.find_next()
+	print("#########next in build_section############3")
+	print(next)
+	print("#########################################")
 	while True:
-		next=header.find_next()
 		if not next:
 			break
+		#Check if next is header or not
 		if re.match("^h", next.name):
-			#section
-			if next.name > header.name:
-				#subsection
-				build_section(next);
+			#check if next is section or sub section
+			if next.name.lower() > header.name.lower():
+				#this header has subsection
+				print("next is subsection")
+				section = build_section(next)
+			else:
+				break
+		else:
+			print("next is not header")
+			next = next.find_next()
+	return section_obj
 
 
 
